70cc7b9c5c17288781c5f9eac7590d153b4b8893
queuebit <queuebit10648@gmail.com>
2021-12-01 13:25:54 -0500

    Add solution for puzzle 1 - 1

---
+++ 2021/days/1/aoc_depth.py
@@ -0,0 +1,31 @@
+#! /usr/local/bin/python3
+
+import sys
+
+year = "2021"
+day = "1"
+problem = "depth"
+print(f"This is the {problem} puzzle.")
+print(f"Detailed instructions can be found here: https://adventofcode.com/{year}/day/{day}")
+
+print(f"> **Given**: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine") 
+print(f"> **Return**: count the number of times a depth measurement increases from the previous measurement")
+print(f"> **NOTE**: There is no measurement before the first measurement.")
+
+data = sys.stdin.readlines()
+
+print(f"{data}")
+d = [int(l.strip()) for l in data]
+
+print(f"d = {d}")
+
+prev = None
+count = 0
+
+for di in d:
+    if prev is not None and di > prev:
+        count += 1
+    prev = di
+
+print(count)
+
