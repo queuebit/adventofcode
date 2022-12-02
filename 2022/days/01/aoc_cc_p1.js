4a12830761e0b649e7a7e788388a125e58082312
queuebit <queuebit10648@gmail.com>
2022-12-01 22:29:43 -0500

    Here's the bad version.

--- 2022/days/01/aoc_cc_p1.js
+++ 2022/days/01/aoc_cc_p1.js
@@ -6,11 +6,25 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
+let elves = 0;
+let elfDiet = 0;
+let appetites = [];
+
 rl.on("line", (line) => {
   console.log(line);
+  if (line.trim() === "") {
+    elves += 1;
+    appetites.push(elfDiet);
+    console.log(`Elf #${elves} Diet: ${elfDiet}`);
+    elfDiet = 0;
+  } else {
+    elfDiet += parseInt(line.trim(), 10);
+  }
 });
 
 rl.once("close", () => {
   console.log("done");
+  console.log(appetites);
+  console.log(Math.max(...appetites));
   // end of input
 });
