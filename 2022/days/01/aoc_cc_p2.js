2ffc5838f462a34b7009b1c4d39f87a4a35fdd56
queuebit <queuebit10648@gmail.com>
2022-12-01 22:54:53 -0500

    Fixed edge case of missing last elf.

--- 2022/days/01/aoc_cc_p2.js
+++ 2022/days/01/aoc_cc_p2.js
@@ -1,3 +1,4 @@
+const { parse } = require("path");
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -10,24 +11,29 @@ let elves = 0;
 let elfDiet = 0;
 let appetites = [];
 
+const elfCount = () => {
+  elves += 1;
+  appetites.push(elfDiet);
+  console.log(`Elf #${elves} Diet: ${elfDiet}`);
+  elfDiet = 0;
+};
+
 rl.on("line", (line) => {
-  console.log(line);
   if (line.trim() === "") {
-    elves += 1;
-    appetites.push(elfDiet);
-    console.log(`Elf #${elves} Diet: ${elfDiet}`);
-    elfDiet = 0;
+    console.log(line);
+    elfCount();
   } else {
     elfDiet += parseInt(line.trim(), 10);
   }
 });
 
 rl.once("close", () => {
+  elfCount();
   console.log("done");
   console.log(appetites);
   const bigAppetiteElves = appetites.sort((a, b) => b - a);
   console.log(bigAppetiteElves);
-  const bigThree = bigAppetiteElves.slice(0, 4);
+  const bigThree = bigAppetiteElves.slice(0, 3);
   const threeTotal = bigThree.reduce((a, b) => a + b, 0);
   console.log(bigThree);
   console.log(threeTotal);
