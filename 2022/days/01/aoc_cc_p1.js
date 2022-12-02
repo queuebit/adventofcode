bcec427eaf7b647ee1660d678f071e736394fba5
queuebit <queuebit10648@gmail.com>
2022-12-01 22:17:21 -0500

    v0 read data.

--- 2022/days/01/aoc_cc_p1.js
+++ 2022/days/01/aoc_cc_p1.js
@@ -1,2 +1,16 @@
-"use strict";
-console.log(3 * 6);
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+rl.on("line", (line) => {
+  console.log(line);
+});
+
+rl.once("close", () => {
+  console.log("done");
+  // end of input
+});
