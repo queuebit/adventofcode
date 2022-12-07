00a4729e60cb0d1bbbcd5ceb91dba497a7eaf26e
queuebit <queuebit10648@gmail.com>
2022-12-07 17:07:10 -0500

    P1, WIP. Missing subdirectory filesizes.

---
+++ 2022/days/07/aoc_nslod_p1.js
@@ -0,0 +1,85 @@
+"use strict";
+var parse = require("path").parse;
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+var COMMANDS = {
+    cd: 0,
+    ls: 1,
+    noop: 99
+};
+var FILETYPES = {
+    directory: 0,
+    file: 1
+};
+var activeCommand = COMMANDS.noop;
+var wd = "?";
+var fs = [
+    { name: "/", filetype: FILETYPES.directory, filesize: 0, parent: "" },
+];
+var handleCommand = function (command) {
+    console.log("COMMAND: ".concat(command));
+    if (command.startsWith("cd")) {
+        var _a = command.split(" "), _ = _a[0], path = _a[1];
+        wd = path;
+        activeCommand = COMMANDS.cd;
+    }
+    else if (command.startsWith("ls")) {
+        activeCommand = COMMANDS.ls;
+    }
+};
+var handleInfo = function (info) {
+    console.log("INFO: ".concat(info));
+    if (activeCommand === COMMANDS.cd) {
+        console.warn("SYNTAX ERROR");
+    }
+    else if (activeCommand === COMMANDS.ls) {
+        if (info.startsWith("dir")) {
+            var _a = info.split(" "), _ = _a[0], dirName = _a[1];
+            fs.push({
+                name: dirName,
+                filetype: FILETYPES.directory,
+                filesize: 0,
+                parent: wd
+            });
+        }
+        else {
+            var _b = info.split(" "), strSize = _b[0], filename = _b[1];
+            var filesize = Number(strSize);
+            fs.push({
+                name: filename,
+                filetype: FILETYPES.file,
+                filesize: filesize,
+                parent: wd
+            });
+        }
+    }
+};
+var dirSizes = function () {
+    var ds = {};
+    return fs.reduceRight(function (prev, cur) {
+        if (!Object.keys(prev).includes(cur.parent)) {
+            prev[cur.parent] = 0;
+        }
+        prev[cur.parent] += cur.filesize;
+        return prev;
+    }, ds);
+};
+rl.on("line", function (line) {
+    if (line.startsWith("$ ")) {
+        handleCommand(line.slice(2));
+    }
+    else {
+        handleInfo(line);
+    }
+});
+rl.once("close", function () {
+    console.log(fs);
+    var ds = dirSizes();
+    console.log(ds);
+    var smallDirs = Object.values(ds).filter(function (x) { return x < 100000; });
+    console.log(smallDirs.reduce(function (a, b) { return a + b; }, 0));
+});
---
+++ 2022/days/07/aoc_nslod_p1.ts
@@ -0,0 +1,94 @@
+const { parse } = require("path");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+let COMMANDS = {
+  cd: 0,
+  ls: 1,
+  noop: 99,
+};
+let FILETYPES = {
+  directory: 0,
+  file: 1,
+};
+type FileInfo = {
+  name: string;
+  filetype: number;
+  filesize: number;
+  parent: string;
+};
+
+let activeCommand = COMMANDS.noop;
+let wd = "?";
+let fs: FileInfo[] = [
+  { name: "/", filetype: FILETYPES.directory, filesize: 0, parent: "" },
+];
+
+const handleCommand = (command: string) => {
+  console.log(`COMMAND: ${command}`);
+  if (command.startsWith("cd")) {
+    const [_, path] = command.split(" ");
+    wd = path;
+    activeCommand = COMMANDS.cd;
+  } else if (command.startsWith("ls")) {
+    activeCommand = COMMANDS.ls;
+  }
+};
+
+const handleInfo = (info: string) => {
+  console.log(`INFO: ${info}`);
+  if (activeCommand === COMMANDS.cd) {
+    console.warn("SYNTAX ERROR");
+  } else if (activeCommand === COMMANDS.ls) {
+    if (info.startsWith("dir")) {
+      const [_, dirName] = info.split(" ");
+      fs.push({
+        name: dirName,
+        filetype: FILETYPES.directory,
+        filesize: 0,
+        parent: wd,
+      });
+    } else {
+      const [strSize, filename] = info.split(" ");
+      const filesize = Number(strSize);
+      fs.push({
+        name: filename,
+        filetype: FILETYPES.file,
+        filesize: filesize,
+        parent: wd,
+      });
+    }
+  }
+};
+
+const dirSizes = () => {
+  let ds: { [key: string]: number } = {};
+  return fs.reduceRight((prev, cur) => {
+    if (!Object.keys(prev).includes(cur.parent)) {
+      prev[cur.parent] = 0;
+    }
+    prev[cur.parent] += cur.filesize;
+    return prev;
+  }, ds);
+};
+
+rl.on("line", (line: string) => {
+  if (line.startsWith("$ ")) {
+    handleCommand(line.slice(2));
+  } else {
+    handleInfo(line);
+  }
+});
+
+rl.once("close", () => {
+  console.log(fs);
+  const ds = dirSizes();
+  console.log(ds);
+  const smallDirs = Object.values(ds).filter((x) => x < 100000);
+  console.log(smallDirs.reduce((a, b) => a + b, 0));
+});
