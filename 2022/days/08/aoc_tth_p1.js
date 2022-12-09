5fc46a87babdf9777d36774cfe717bf4bce73157
queuebit <queuebit10648@gmail.com>
2022-12-08 22:32:04 -0500

    Break out filters (still wrong).

--- 2022/days/08/aoc_tth_p1.js
+++ 2022/days/08/aoc_tth_p1.js
@@ -26,24 +26,42 @@ rl.once("close", function () {
     console.log(treeCols);
     var rs = treeRows.length;
     var cs = treeCols.length;
-    var inViewMap = {};
-    for (var r = 0; r < rs; r++) {
-        var _loop_1 = function (c) {
-            var key = "".concat(r).concat(c);
+    var inViewMap = [];
+    var inViewLookup = {};
+    for (var c = 0; c < cs; c++) {
+        inViewMap.push([]);
+        var _loop_1 = function (r) {
+            var key = "".concat(c).concat(r);
             if (r === 0 || c === 0 || r === rs - 1 || c === cs - 1) {
-                inViewMap[key] = true;
+                inViewMap[c].push(true);
+                inViewLookup[key] = true;
             }
-            var treeHeight = treeRows[c][r];
-            var leftView = !(treeRows[c].slice(0, r).filter(function (t) { return t >= treeHeight; }).length > 0);
-            var rightView = !(treeRows[c].slice(r + 1).filter(function (t) { return t >= treeHeight; }).length > 0);
-            var upView = !(treeCols[r].slice(0, c).filter(function (t) { return t >= treeHeight; }).length > 0);
-            var downView = !(treeCols[r].slice(c + 1).filter(function (t) { return t >= treeHeight; }).length > 0);
-            inViewMap[key] = leftView || rightView || upView || downView;
+            else {
+                var treeHeight_1 = treeRows[c][r];
+                var leftVals = treeRows[c].slice(0, r);
+                var leftView = !(leftVals.filter(function (t) { return t >= treeHeight_1; }).length > 0);
+                // console.log({ key, treeHeight, leftVals, leftView });
+                var rightVals = treeRows[c].slice(r + 1);
+                var rightView = !(rightVals.filter(function (t) { return t >= treeHeight_1; }).length > 0);
+                // console.log({ key, treeHeight, rightVals, rightView });
+                var upVals = treeCols[r].slice(0, c);
+                var upView = !(upVals.filter(function (t) { return t >= treeHeight_1; }).length > 0);
+                // console.log({ key, treeHeight, upVals, upView });
+                var downVals = treeCols[r].slice(c + 1);
+                var downView = !(downVals.filter(function (t) { return t >= treeHeight_1; }).length > 0);
+                // console.log({ key, treeHeight, downVals, downView });
+                inViewMap[c].push(leftView || rightView || upView || downView);
+                inViewLookup[key] = leftView || rightView || upView || downView;
+            }
         };
-        for (var c = 0; c < cs; c++) {
-            _loop_1(c);
+        for (var r = 0; r < rs; r++) {
+            _loop_1(r);
         }
     }
     console.log(inViewMap);
-    console.log(Object.values(inViewMap).filter(function (viewable) { return viewable; }).length);
+    console.log({ rs: rs, cs: cs });
+    console.log(Object.values(inViewLookup).filter(function (viewable) { return viewable; }).length);
+    /* That's not the right answer; your answer is too low.
+    If you're stuck, make sure you're using the full input data; there are also some general tips on the about page,
+    or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 1613.) [Return to Day 8] */
 });
--- 2022/days/08/aoc_tth_p1.ts
+++ 2022/days/08/aoc_tth_p1.ts
@@ -30,32 +30,50 @@ rl.once("close", () => {
 
   const rs = treeRows.length;
   const cs = treeCols.length;
-  let inViewMap: { [key: string]: boolean } = {};
-  for (let r = 0; r < rs; r++) {
-    for (let c = 0; c < cs; c++) {
-      const key = `${r}${c}`;
+  let inViewMap: boolean[][] = [];
+  let inViewLookup: { [key: string]: boolean } = {};
+  for (let c = 0; c < cs; c++) {
+    inViewMap.push([]);
+    for (let r = 0; r < rs; r++) {
+      const key = `${c}${r}`;
       if (r === 0 || c === 0 || r === rs - 1 || c === cs - 1) {
-        inViewMap[key] = true;
+        inViewMap[c].push(true);
+        inViewLookup[key] = true;
+      } else {
+        const treeHeight = treeRows[c][r];
+
+        const leftVals = treeRows[c].slice(0, r);
+        const leftView = !(leftVals.filter((t) => t >= treeHeight).length > 0);
+        // console.log({ key, treeHeight, leftVals, leftView });
+
+        const rightVals = treeRows[c].slice(r + 1);
+        const rightView = !(
+          rightVals.filter((t) => t >= treeHeight).length > 0
+        );
+        // console.log({ key, treeHeight, rightVals, rightView });
+
+        const upVals = treeCols[r].slice(0, c);
+        const upView = !(upVals.filter((t) => t >= treeHeight).length > 0);
+        // console.log({ key, treeHeight, upVals, upView });
+
+        const downVals = treeCols[r].slice(c + 1);
+        const downView = !(downVals.filter((t) => t >= treeHeight).length > 0);
+        // console.log({ key, treeHeight, downVals, downView });
+
+        inViewMap[c].push(leftView || rightView || upView || downView);
+        inViewLookup[key] = leftView || rightView || upView || downView;
       }
-      const treeHeight = treeRows[c][r];
-      const leftView = !(
-        treeRows[c].slice(0, r).filter((t) => t >= treeHeight).length > 0
-      );
-      const rightView = !(
-        treeRows[c].slice(r + 1).filter((t) => t >= treeHeight).length > 0
-      );
-      const upView = !(
-        treeCols[r].slice(0, c).filter((t) => t >= treeHeight).length > 0
-      );
-      const downView = !(
-        treeCols[r].slice(c + 1).filter((t) => t >= treeHeight).length > 0
-      );
-      inViewMap[key] = leftView || rightView || upView || downView;
     }
   }
   console.log(inViewMap);
-  console.log(Object.values(inViewMap).filter((viewable) => viewable).length);
+  console.log({ rs, cs });
+  console.log(
+    Object.values(inViewLookup).filter((viewable) => viewable).length
+  );
   /* That's not the right answer; your answer is too low.
   If you're stuck, make sure you're using the full input data; there are also some general tips on the about page,
   or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 1613.) [Return to Day 8] */
+  /* That's not the right answer; your answer is too low.
+  If you're stuck, make sure you're using the full input data; there are also some general tips on the about page,
+  or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 1611.) [Return to Day 8] */
 });
