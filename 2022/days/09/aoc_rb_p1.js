ec8661f18d2265a1b3bc9fa9035f03a2f956fa9c
queuebit <queuebit10648@gmail.com>
2022-12-09 11:22:31 -0500

    Here's the bad version, with biz rules hardcoded.

---
+++ 2022/days/09/aoc_rb_p1.js
@@ -0,0 +1,66 @@
+"use strict";
+var parse = require("path").parse;
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+var instructions = [];
+rl.on("line", function (line) {
+    var _a = line.split(" "), move = _a[0], count = _a[1];
+    instructions.push([move, Number(count)]);
+});
+var MOVES = {
+    R: [1, 0],
+    L: [-1, 0],
+    U: [0, 1],
+    D: [0, -1]
+};
+var FOLLOW_MOVE = {
+    "02": [0, 1],
+    "20": [1, 0],
+    "0-2": [0, -1],
+    "-20": [-1, 0],
+    "21": [1, 1],
+    "12": [1, 1],
+    "1-2": [1, -1],
+    "2-1": [1, -1],
+    "-21": [-1, 1],
+    "-12": [-1, 1],
+    "-1-2": [-1, -1],
+    "-2-1": [-1, -1]
+};
+var start = [0, 0];
+var startKey = "0-0";
+var hx = start[0], hy = start[1];
+var tx = start[0], ty = start[1];
+var headVisited = new Set([startKey]);
+var tailVisited = new Set([startKey]);
+var nearBy = function (head, tail) {
+    return Math.hypot(hy - ty, hx - tx) < 2;
+};
+var makeMoves = function (instructions) {
+    instructions.forEach(function (_a) {
+        var move = _a[0], count = _a[1];
+        var _b = MOVES[move], dx = _b[0], dy = _b[1];
+        for (var i = 0; i < count; i++) {
+            hx += dx;
+            hy += dy;
+            headVisited.add("".concat(hx, "-").concat(hy));
+            if (!nearBy([hx, hy], [tx, ty])) {
+                var sepx = hx - tx;
+                var sepy = hy - ty;
+                var _c = FOLLOW_MOVE["".concat(sepx).concat(sepy)], tmx = _c[0], tmy = _c[1];
+                tx += tmx;
+                ty += tmy;
+                tailVisited.add("".concat(tx, "-").concat(ty));
+            }
+        }
+    });
+};
+rl.once("close", function () {
+    makeMoves(instructions);
+    console.log(tailVisited);
+    console.log(tailVisited.size);
+});
---
+++ 2022/days/09/aoc_rb_p1.ts
@@ -0,0 +1,72 @@
+const { parse } = require("path");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+let instructions: [move: string, count: number][] = [];
+rl.on("line", (line: string) => {
+  const [move, count] = line.split(" ");
+  instructions.push([move, Number(count)]);
+});
+
+const MOVES: { [key: string]: [nx: number, ny: number] } = {
+  R: [1, 0],
+  L: [-1, 0],
+  U: [0, 1],
+  D: [0, -1],
+};
+const FOLLOW_MOVE: { [key: string]: [nx: number, ny: number] } = {
+  "02": [0, 1],
+  "20": [1, 0],
+  "0-2": [0, -1],
+  "-20": [-1, 0],
+  "21": [1, 1],
+  "12": [1, 1],
+  "1-2": [1, -1],
+  "2-1": [1, -1],
+  "-21": [-1, 1],
+  "-12": [-1, 1],
+  "-1-2": [-1, -1],
+  "-2-1": [-1, -1],
+};
+let start = [0, 0];
+let startKey = `0-0`;
+let [hx, hy] = start;
+let [tx, ty] = start;
+let headVisited = new Set([startKey]);
+let tailVisited = new Set([startKey]);
+
+const nearBy = (
+  head: [hx: number, hy: number],
+  tail: [tx: number, ty: number]
+) => {
+  return Math.hypot(hy - ty, hx - tx) < 2;
+};
+const makeMoves = (instructions: [move: string, count: number][]) => {
+  instructions.forEach(([move, count]) => {
+    let [dx, dy] = MOVES[move];
+    for (let i = 0; i < count; i++) {
+      hx += dx;
+      hy += dy;
+      headVisited.add(`${hx}-${hy}`);
+
+      if (!nearBy([hx, hy], [tx, ty])) {
+        const sepx = hx - tx;
+        const sepy = hy - ty;
+        const [tmx, tmy] = FOLLOW_MOVE[`${sepx}${sepy}`];
+        tx += tmx;
+        ty += tmy;
+        tailVisited.add(`${tx}-${ty}`);
+      }
+    }
+  });
+};
+rl.once("close", () => {
+  makeMoves(instructions);
+  console.log(tailVisited);
+  console.log(tailVisited.size);
+});
