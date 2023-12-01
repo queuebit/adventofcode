b26cfa001a3cf74c365b1351c09f9d522e57ea87
queuebit <queuebit10648@gmail.com>
2023-12-01 15:44:14 -0500

    Finish day1 part2.

--- 2023/days/01/aoc_trebuchet.js
+++ 2023/days/01/aoc_trebuchet.js
@@ -6,8 +6,32 @@ var rl = readline.createInterface({
     output: process.stdout,
     terminal: false
 });
+var DIGITS = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+};
 var getN = function (s) {
     return Number(s);
+};
+var spelledOut = function (s) {
+    if (Number.isInteger(DIGITS[s])) {
+        return Number(DIGITS[s]);
+    }
+    else {
+        if (Object.keys(DIGITS).some(function (d) { return d.startsWith(s) || d.endsWith(s); })) {
+            return s;
+        }
+        else {
+            return;
+        }
+    }
 };
 var part1 = function () {
     return input
@@ -34,7 +58,53 @@ var part1 = function () {
         return acc + v;
     }, 0);
 };
-var part2 = function () { };
+var part2 = function () {
+    return input
+        .map(function (line) {
+        console.log(line);
+        var o = 0;
+        var first;
+        var last;
+        var l = line.length;
+        var fWord = "";
+        var bWord = "";
+        while (typeof first === "undefined" || typeof last === "undefined") {
+            var ac = line.charAt(o);
+            var bc = line.charAt(l - o - 1);
+            var a = getN(ac);
+            var b = getN(bc);
+            fWord = "".concat(fWord).concat(ac);
+            bWord = "".concat(bc).concat(bWord);
+            if (typeof first === "undefined") {
+                if (Number.isInteger(a)) {
+                    first = a;
+                }
+                else if (Number.isInteger(spelledOut(fWord))) {
+                    first = Number(spelledOut(fWord));
+                }
+                else {
+                    fWord = spelledOut(fWord) || fWord.slice(1);
+                }
+            }
+            if (typeof last === "undefined") {
+                if (Number.isInteger(b)) {
+                    last = b;
+                }
+                else if (Number.isInteger(spelledOut(bWord))) {
+                    last = Number(spelledOut(bWord));
+                }
+                else {
+                    bWord = spelledOut(bWord) || bc;
+                }
+            }
+            o += 1;
+        }
+        return Number("".concat(first).concat(last));
+    })
+        .reduce(function (acc, v) {
+        return acc + v;
+    }, 0);
+};
 var input = [];
 var loadLine = function (line) {
     input.push(line);
@@ -44,5 +114,6 @@ rl.on("line", function (line) {
 });
 rl.once("close", function () {
     console.log(part1());
-    part2();
+    console.log();
+    console.log(part2());
 });
--- 2023/days/01/aoc_trebuchet.ts
+++ 2023/days/01/aoc_trebuchet.ts
@@ -6,8 +6,32 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
+const DIGITS: { [key: string]: number | undefined } = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
 const getN = (s: string) => {
   return Number(s);
+};
+
+const spelledOut = (s: string): string | number | undefined => {
+  if (Number.isInteger(DIGITS[s])) {
+    return Number(DIGITS[s]);
+  } else {
+    if (Object.keys(DIGITS).some((d) => d.startsWith(s) || d.endsWith(s))) {
+      return s;
+    } else {
+      return;
+    }
+  }
 };
 
 const part1 = () => {
@@ -37,7 +61,55 @@ const part1 = () => {
       return acc + v;
     }, 0);
 };
-const part2 = () => {};
+const part2 = () => {
+  return input
+    .map((line) => {
+      console.log(line);
+
+      let o: number = 0;
+      let first: number | undefined;
+      let last: number | undefined;
+      let l = line.length;
+
+      let fWord: string | number = "";
+      let bWord: string | number = "";
+      while (typeof first === "undefined" || typeof last === "undefined") {
+        const ac = line.charAt(o);
+        const bc = line.charAt(l - o - 1);
+
+        const a = getN(ac);
+        const b = getN(bc);
+
+        fWord = `${fWord}${ac}`;
+        bWord = `${bc}${bWord}`;
+
+        if (typeof first === "undefined") {
+          if (Number.isInteger(a)) {
+            first = a;
+          } else if (Number.isInteger(spelledOut(fWord))) {
+            first = Number(spelledOut(fWord));
+          } else {
+            fWord = spelledOut(fWord) || fWord.slice(1);
+          }
+        }
+        if (typeof last === "undefined") {
+          if (Number.isInteger(b)) {
+            last = b;
+          } else if (Number.isInteger(spelledOut(bWord))) {
+            last = Number(spelledOut(bWord));
+          } else {
+            bWord = spelledOut(bWord) || bc;
+          }
+        }
+
+        o += 1;
+      }
+      return Number(`${first}${last}`);
+    })
+    .reduce((acc, v) => {
+      return acc + v;
+    }, 0);
+};
 
 let input: string[] = [];
 const loadLine = (line: string) => {
@@ -50,5 +122,6 @@ rl.on("line", (line: string) => {
 
 rl.once("close", () => {
   console.log(part1());
-  part2();
+  console.log();
+  console.log(part2());
 });
