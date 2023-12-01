7f69df763dceb7b6532b5fc4a34d0d00bffc162c
queuebit <queuebit10648@gmail.com>
2023-12-01 14:33:32 -0500

    Day1 - pt 1

--- 2023/days/01/aoc_trebuchet.js
+++ 2023/days/01/aoc_trebuchet.js
@@ -6,12 +6,43 @@ var rl = readline.createInterface({
     output: process.stdout,
     terminal: false
 });
-var part1 = function () { };
+var getN = function (s) {
+    return Number(s);
+};
+var part1 = function () {
+    return input
+        .map(function (line) {
+        console.log(line);
+        var o = 0;
+        var first;
+        var last;
+        var l = line.length;
+        while (typeof first === "undefined" || typeof last === "undefined") {
+            var a = getN(line.charAt(o));
+            var b = getN(line.charAt(l - o - 1));
+            if (typeof first === "undefined" && Number.isInteger(a)) {
+                first = a;
+            }
+            if (typeof last === "undefined" && Number.isInteger(b)) {
+                last = b;
+            }
+            o += 1;
+        }
+        return Number("".concat(first).concat(last));
+    })
+        .reduce(function (acc, v) {
+        return acc + v;
+    }, 0);
+};
 var part2 = function () { };
+var input = [];
+var loadLine = function (line) {
+    input.push(line);
+};
 rl.on("line", function (line) {
-    console.log(line);
+    loadLine(line);
 });
 rl.once("close", function () {
-    part1();
+    console.log(part1());
     part2();
 });
--- 2023/days/01/aoc_trebuchet.ts
+++ 2023/days/01/aoc_trebuchet.ts
@@ -6,14 +6,49 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
-const part1 = () => {};
+const getN = (s: string) => {
+  return Number(s);
+};
+
+const part1 = () => {
+  return input
+    .map((line) => {
+      console.log(line);
+
+      let o: number = 0;
+      let first: number | undefined;
+      let last: number | undefined;
+      let l = line.length;
+      while (typeof first === "undefined" || typeof last === "undefined") {
+        const a = getN(line.charAt(o));
+        const b = getN(line.charAt(l - o - 1));
+        if (typeof first === "undefined" && Number.isInteger(a)) {
+          first = a;
+        }
+        if (typeof last === "undefined" && Number.isInteger(b)) {
+          last = b;
+        }
+
+        o += 1;
+      }
+      return Number(`${first}${last}`);
+    })
+    .reduce((acc, v) => {
+      return acc + v;
+    }, 0);
+};
 const part2 = () => {};
 
+let input: string[] = [];
+const loadLine = (line: string) => {
+  input.push(line);
+};
+
 rl.on("line", (line: string) => {
-  console.log(line);
+  loadLine(line);
 });
 
 rl.once("close", () => {
-  part1();
+  console.log(part1());
   part2();
 });
