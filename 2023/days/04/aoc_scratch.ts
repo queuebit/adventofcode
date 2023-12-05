e30ebe89f69bdf5b24515147edb43bef8da90898
queuebit <queuebit10648@gmail.com>
2023-12-05 18:51:05 -0500

    Pencil in days 04 and 05.

---
+++ 2023/days/04/aoc_scratch.ts
@@ -0,0 +1,19 @@
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+const part1 = () => {};
+const part2 = () => {};
+
+rl.on("line", (line: string) => {
+  console.log(line);
+});
+
+rl.once("close", () => {
+  part1();
+  part2();
+});
