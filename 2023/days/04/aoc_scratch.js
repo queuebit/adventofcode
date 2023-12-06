378a977c4261322442e8fa1fd05a7dc7867d8899
queuebit <queuebit10648@gmail.com>
2023-12-06 09:24:20 -0500

    Day 4 part 2

--- 2023/days/04/aoc_scratch.js
+++ 2023/days/04/aoc_scratch.js
@@ -28,12 +28,28 @@ var part1 = function () {
     console.log(scores);
     console.log(scores.reduce(function (acc, s) { return acc + s; }, 0));
 };
-var part2 = function () { };
+var part2 = function () {
+    var cardCopies = new Map();
+    cards.forEach(function (c) { return cardCopies.set(c.n, 1); });
+    cards.forEach(function (c) {
+        var matches = Array.from(c.winningNumbers).filter(function (wn) {
+            return c.cardNumbers.has(wn);
+        }).length;
+        var thisCardCopies = cardCopies.get(c.n) || 0;
+        for (var i = 1; i <= matches; i++) {
+            var before = cardCopies.get(c.n + i) || 0;
+            cardCopies.set(c.n + i, before + thisCardCopies);
+        }
+        console.log(Array.from(cardCopies.values()));
+    });
+    console.log(Array.from(cardCopies.values()).reduce(function (acc, s) { return acc + s; }, 0));
+};
 rl.on("line", function (line) {
     console.log(line);
     readScratchoffCard(line);
 });
 rl.once("close", function () {
     part1();
+    console.log();
     part2();
 });
--- 2023/days/04/aoc_scratch.ts
+++ 2023/days/04/aoc_scratch.ts
@@ -37,7 +37,24 @@ const part1 = () => {
   console.log(scores);
   console.log(scores.reduce((acc, s) => acc + s, 0));
 };
-const part2 = () => {};
+const part2 = () => {
+  const cardCopies: Map<number, number> = new Map();
+  cards.forEach((c) => cardCopies.set(c.n, 1));
+
+  cards.forEach((c) => {
+    const matches = Array.from(c.winningNumbers).filter((wn) =>
+      c.cardNumbers.has(wn)
+    ).length;
+
+    const thisCardCopies = cardCopies.get(c.n) || 0;
+    for (let i = 1; i <= matches; i++) {
+      const before = cardCopies.get(c.n + i) || 0;
+      cardCopies.set(c.n + i, before + thisCardCopies);
+    }
+    console.log(Array.from(cardCopies.values()));
+  });
+  console.log(Array.from(cardCopies.values()).reduce((acc, s) => acc + s, 0));
+};
 
 rl.on("line", (line: string) => {
   console.log(line);
@@ -46,5 +63,6 @@ rl.on("line", (line: string) => {
 
 rl.once("close", () => {
   part1();
+  console.log();
   part2();
 });
